c6aef34a758ff3aa9bc18eacea6cd6685f0b3c76
sakusaku000 <harry.spncr@gmail.com>
2022-06-14 17:57:54 +0100

    back to library button on any non-library page

--- src/components/Layout/MainAppLayout/Logo.vue
+++ src/components/Layout/MainAppLayout/Logo.vue
@@ -1,15 +1,25 @@
 <template>
-    <div id="steam-logo">
+    <div id="steam-logo" class="flex items-center gap-4">
+        <div id="back-button" v-if="$route.path !== '/'">
+            <Back class="w-7 text-slate-600 hover:text-slate-500 duration-100 cursor-pointer" @click="BackToLibrary"/>
+        </div>
         <Steam class="w-12 text-slate-500"/>
     </div>
 </template>
 
 <script>
 import Steam from '../../Icons/SteamMain.vue';
+import Back from '../../Icons/Back.vue';
 export default {
     name:"TitleBarLogo",
     components:{
-        Steam
+        Steam,
+        Back
+    },
+    methods:{
+        BackToLibrary() {
+            this.$router.push("/");
+        }
     }
 }
 </script>
--- src/components/Layout/MainAppLayout/TitleBar.vue
+++ src/components/Layout/MainAppLayout/TitleBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center justify-between px-14 pt-14">
+    <div class="flex items-center justify-between px-14 pt-14 select-none">
         <Logo/>
         <User/>
     </div>
