cb10ac1b8335f7fe071e7174d26c5708fc17da48
sakusaku000 <harry.spncr@gmail.com>
2022-06-14 17:35:32 +0100

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import store from './store/main.js';
 import router from './router';
 
 // CSS
 import './assets/css/tailwind.css';
 import './assets/css/main.css';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
